1044d709f69394e3d24d79cb99a1412d6439ed4c
ghostiek <patricktawil1@hotmail.com>
2024-06-30 23:15:23 -0400

    remove useless endpoint

--- api/main.py
+++ api/main.py
@@ -52,17 +52,6 @@ async def create_user(user: UserSchema.UserCreationInput, db: Session = Depends(
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
-
-@app.post("/users/login", response_model=UserSchema.User)
-async def login_user(user: UserSchema.User,
-                     db: Session = Depends(get_db)
-                     ):
-    db_user = crud.get_user(db, user=user)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="Incorrect username or password")
-    # Create JWT and send it back
-    return
-
 @app.post("/token")
 async def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
